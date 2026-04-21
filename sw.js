01f64132a8067fe7c16c6b4093c886dc4e447bfc
chanel07 <82330098+chanel07@users.noreply.github.com>
2026-04-21 14:44:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const CACHE = 'huellaviva-v1';
-const ASSETS = ['/', '/index.html'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+const CACHE_NAME = "huellaviva-v2";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
